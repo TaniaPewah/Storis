786f01c3c1f66e3bb28fc10b809be51e0858c96d
Tania <drob.tania@gmail.com>
2021-05-31 16:13:06 +0300

    /create (edit story component updated) TODO: disable submit button if user not logged in, continue existing story

--- src/app/components/edit-story/edit-story.component.ts
+++ src/app/components/edit-story/edit-story.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, EventEmitter, Output, Input,ElementRef, ViewChild } from '@angular/core';
 import { User } from 'src/app/models/User';
 import { Story } from '../../models/Story';
-import { StoryService } from '../../services/story.service';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { StoryService } from 'src/app/services/story.service';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-edit-story',
@@ -9,15 +13,29 @@ import { StoryService } from '../../services/story.service';
   styleUrls: ['./edit-story.component.css']
 })
 export class EditStoryComponent implements OnInit {
-  @Input() user: User;
+
   @Output() cancelCreateStory: EventEmitter<any> = new EventEmitter();
   @ViewChild('author') author: ElementRef;
 
+  user: User = new User();
   story: Story = new Story();
   submitted = false;
   imageToUpload: File = null;
 
-  constructor(private storyService:StoryService ) { }
+  constructor(private storyService:StoryService,
+    private authService: AuthService,
+              private afAuth: AngularFireAuth,
+              private router: Router ) { 
+    this.afAuth.onAuthStateChanged(auth => {
+      if (auth !== undefined && auth !== null) {
+        this.user = auth;
+        console.log("user logged in");
+      } else {
+        console.log("user not logged in");
+        debugger;
+      }
+    });
+  }
 
   ngOnInit(): void {    
   }
@@ -30,14 +48,9 @@ export class EditStoryComponent implements OnInit {
   onSubmit(){
   
     this.story.author = this.author.nativeElement.innerHTML;
-
     this.story.date = new Date();
     this.storyService.saveStory(this.story, this.imageToUpload);
-  }
-
-  newStory(): void {
-    this.submitted = false;
-    this.story = new Story();
+    this.router.navigate(['/browse']);
   }
 
   handleImageFileInput( event ){
