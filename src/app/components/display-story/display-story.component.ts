1f96a0fc6d183a2cfa35cfbba78b79587bf07094
Tania <drob.tania@gmail.com>
2021-06-10 17:19:59 +0300

    updated edit mode on the same story after chosing an option, not finished saving

--- src/app/components/display-story/display-story.component.ts
+++ src/app/components/display-story/display-story.component.ts
@@ -11,6 +11,8 @@ export class DisplayStoryComponent implements OnInit {
   @Input() story: Story;
   @Output() chooseStoryOption: EventEmitter<any> = new EventEmitter();
   currentStory: Story = null;
+  editing: boolean = false;
+  imageToUpload: File = null;
 
   constructor( private storyService:StoryService ) {
    }
@@ -23,7 +25,41 @@ export class DisplayStoryComponent implements OnInit {
   }
 
   makeChoice(target): void{
-    this.chooseStoryOption.emit();
+    // TODO: get the clicked choice
+    let chosenChoice = "this";
+    // if chosen option has content:
+    //   display the content on the same component
+    // else open edit mode on the same component
+    if( this.story.choice_one.content ){
+      this.displayNextOutcome();
+    } else {
+      this.openEditChoiceMode( chosenChoice );
+    }
   }
 
+  displayNextOutcome(){
+    // TODO: sliding effect of the prev story, 
+    // display the chosen choice.content on the same component
+  }
+
+  openEditChoiceMode( choiceText ){
+
+    this.editing = true;
+    console.log("opening edit choice mode");
+    // TODO: sliding effect of the prev story, title becomes the chosen choice text
+    this.story = new Story();
+    this.story.title = choiceText;
+  }
+
+  handleImageFileInput( event ){
+    this.imageToUpload = event.target.files[0];
+  }
+
+  onSubmit(){
+  
+    // this.story.author = this.user.displayName;
+    // this.story.date = new Date();
+    // this.storyService.saveStory(this.story, this.imageToUpload);
+    // this.router.navigate(['/browse']);
+  }
 }
--- src/app/models/Story.ts
+++ src/app/models/Story.ts
@@ -1,11 +1,13 @@
+import { Optional } from '@angular/core'; 
+
 export class Story{
     id: string;
     author: string;
     title: string;
     text: string;
     image: string;
-    choice_one: string;
-    choice_two: string;
+    choice_one: any;
+    choice_two: any;
     father: number;
     date: Date;
     pulse: number;
